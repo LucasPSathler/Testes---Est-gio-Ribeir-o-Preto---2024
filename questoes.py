7bf9736acea52a03aedb4015d174803b9faa1db2
Lucas Pires Sathler <140682530+LucasPSathler@users.noreply.github.com>
2024-03-21 16:02:29 -0300

    Add files via upload

---
+++ questoes.py
@@ -0,0 +1,79 @@
+"""
+ESTÁGIO RIBEIRÃO PRETO - 2024
+
+OBS: Todas as respostas estão comentadas, para visulizar, basta retirar as aspas triplas do inicio e fim do código.
+
+
+ QUESTÃO - 01
+
+ indice = 13
+ soma = 0
+ k = 0
+
+ while k < indice:
+
+     k += 1
+     soma += k
+     print(soma)
+
+
+QUESTÃO - 02
+
+
+Infelizmente não consegui realizar este desafio    
+
+
+    
+
+
+
+
+QUESTÃO - 03
+# Descubra a lógica e complete o próximo elemento:
+
+
+a) 1, 3, 5, 7, PROXIMO ELEMENTO -> 9
+
+b) 2, 4, 8, 16, 32, 64, PROXIMO ELEMENTO -> 128
+
+c) 0, 1, 4, 9, 16, 25, 36, PROXIMO ELEMENTO -> 49
+
+d) 4, 16, 36, 64, PROXIMO ELEMENTO -> 100
+
+e) 1, 1, 2, 3, 5, 8, PROXIMO ELEMENTO -> 13
+
+f) 2,10, 12, 16, 17, 18, 19, PROXIMO ELEMENTO -> 200
+
+
+
+
+QUESTÃO - 04
+
+
+Inicialmente, ligo o primeiro interruptor por dez minutos. Aguardo esses dez minutos
+e o desligo, e ligo o segundo interruptor. O segundo interruptor permanece ligado e eu vou para a primeira sala. 
+
+Caso a luz esteja apagada, mas quente, significa que o o primeiro interruptor é da primera sala. 
+
+Caso esteja a luz esteja acesa, significa que o segundo interruptor é da primeira sala. 
+
+Caso esteja apagada e fria, significa que o terceiro interruptor corresponde a primeira sala.
+
+Retorno e visito a segunda sala, para repetir o processo de conferência e eliminar mais uma opção.
+
+
+
+
+
+
+QUESTÃO - 05
+
+Escreva um programa que inverta os caracteres de um string.
+
+
+invertida = input("Por favor, escreva uma palavra/frase: ")
+
+print(f'Sua palavra/frase invertida é: ', invertida[::-1])  #Foi utilizado a técnica de fatiamento
+
+
+"""
